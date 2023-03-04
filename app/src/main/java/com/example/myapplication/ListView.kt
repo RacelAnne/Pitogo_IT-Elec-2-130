ff42e1b900aba1cfa093e91a392127280db73bff
Admin <racelanne19@gmail.com>
2023-03-04 18:05:07 +0800

    List View Activity 03-04-23

---
+++ app/src/main/java/com/example/myapplication/ListView.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import androidx.appcompat.app.AppCompatActivity
+
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+var array = arrayOf("Android", "Java", "Php", "Hadoop", "Sap", "Python", "Ajax", "C++", "Ruby", "Rails", ".Net", "Perl")
+class ListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+
+        val adapter = ArrayAdapter(this,
+            R.layout.activity_my_list, array)
+
+        val listView:ListView = findViewById(R.id.listView)
+        listView.setAdapter(adapter)
+
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(parent: AdapterView<*>, view: View,
+                                     position: Int, id: Long) {
+
+                // value of item that is clicked
+                val itemValue = listView.getItemAtPosition(position) as String
+
+                // Toast the values
+                Toast.makeText(applicationContext,
+                    "Position :$position\nItem Value : $itemValue", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+}
