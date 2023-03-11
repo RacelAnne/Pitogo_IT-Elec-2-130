63001bb7f128229d594b84a4c6f3192e77a2e05a
Admin <racelanne19@gmail.com>
2023-03-11 18:45:47 +0800

    Fragment Activity 03-11-23

--- app/src/main/java/com/example/myapplication/MainActivityForFragment.kt
+++ app/src/main/java/com/example/myapplication/MainActivityForFragment.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivityForFragment : AppCompatActivity() {
     private val fragmentManager = supportFragmentManager
@@ -10,18 +11,20 @@ class MainActivityForFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_for_fragment)
+
+
     }
 
     fun onClickFirst(view: View) {
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.frameLayout, FirstFragment())
+        fragmentTransaction.add(R.id.frameLayout, FragmentA())
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
     }
 
     fun onClickSecond(view: View) {
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.frameLayout, SecondFragment())
+        fragmentTransaction.add(R.id.frameLayout, FragmentB())
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
     }
